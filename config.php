db8dc053c2ae7bb6a31ab7b1cbf5abc5e7a16ff8
ptomas-adacis <87819933+ptomas-adacis@users.noreply.github.com>
2022-10-06 12:35:52 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$url = $_SERVER['EDCS_ENV_TEST_URL'];
+$path = $_SERVER['EDCS_ENV_TEST_PATH'];
+$user = $_SERVER['EDCS_ENV_TEST_USER'];
+
+printf("url = '%s'\n", $url);
+printf("path = '%s'\n", $path);
+printf("user = '%s'\n", $user);
+
+?>
